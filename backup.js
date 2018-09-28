dfb15bcf2ecb38c1ea36f17a78c9d3b41f6278e0
jarontai <jaroncn@gmail.com>
2018-09-28 10:00:33 +0800

    Add logging

--- backup.js
+++ backup.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
 const Promise = require('bluebird');
+const SimpleNodeLogger = require('simple-node-logger');
 const SSH = require('node-ssh');
 
 const ssh = new SSH();
@@ -22,7 +23,11 @@ fs.readdirSync(localFolder).forEach(file => {
   backupFiles[file] = true;
 })
 
-console.log('Discourse backup begin!');
+const logger = SimpleNodeLogger.createSimpleLogger({
+  logFilePath: path.join(localFolder, 'backup.log'),
+  timestampFormat: 'YYYY-MM-DD HH:mm:ss',
+});
+logger.info('Backup begin!');
 
 Promise.delay(6000).then(() => {
   ssh.connect({
@@ -30,27 +35,31 @@ Promise.delay(6000).then(() => {
     username: process.env.SSH_USER,
     privateKey: process.env.PRIVATE_KEY,
   }).then(() => {
-    ssh.execCommand('ls', { cwd: remoteFolder }).then(function (result) {
+    ssh.execCommand('ls', { cwd: remoteFolder }).then((result) => {
       const stdout = result.stdout || '';
       const stderr = result.stderr;
       if (stderr) {
-        console.error(stderr);
+        loggr.error('ls error', stderr);
         return process.exit();
       }
 
       const files = stdout.split(/\r?\n/) || [];
-      Promise.each(files, function (file) {
+      Promise.each(files, (file) => {
         if (_.endsWith(file, extension) && !backupFiles[file]) {
-          console.log(file);
+          logger.info('Backup file', file);
           return ssh.getFile(path.join(localFolder, file), path.join(remoteFolder, file));
+        } else {
+          return Promise.resolve();
         }
-      }).then(function () {
-        console.log('Discourse backup finished!');
-        process.exit();
+      }).then((data) => {
+        logger.info('Backup finished!');
+        setTimeout(() => {
+          process.exit();
+        }, 2000);
       });
     });
   }, (error) => {
-    console.error('Discourse backup error', error);
+    logger.error('SSH error', error);
     process.exit();
   });
 });
